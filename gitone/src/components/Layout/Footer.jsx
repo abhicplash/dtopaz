68292aefbe20b9ff6f446dc37502ed7a745d5a38
Abhilash CP <abhicplash@gmail.com>
2024-06-11 23:45:24 +0530

    ab corrections -11 june

--- gitone/src/components/Layout/Footer.jsx
+++ gitone/src/components/Layout/Footer.jsx
@@ -2,26 +2,28 @@ import React from "react";
 import { IoLocation, IoCall } from "react-icons/io5";
 import { FaEnvelope } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import logo from "../../Assets/logo/logo.png";
 
 function Footer() {
   return (
     <div
       className="bg-[#2e728f] text-[#a7babb]  
-    font-Numbers border-t border-[#e0d5af] w-full md:h-72   md:flex-row flex-col
+    font-Numbers border-t border-[#e0d5af] w-full md:h-80   md:flex-row flex-col
     flex justify-between md:px-32 items-center gap-10 py-12 md:py-0 "
     >
-      <div className="flex-col flex gap-3">
+      <div className="flex-col flex  justify-center items-center">
         <Link to={"/"}>
-          <h1 className="text-4xl font-bold text-[#c08f52] text-center md:text-left">
+          <img src={logo} alt="" className="w-40 h-36 " />
+          {/* <h1 className="text-4xl font-bold text-[#c08f52] text-center md:text-left">
             Diana Topaz
-          </h1>
+          </h1> */}
         </Link>
-        <p className="w-64 text-[#a7babb] text-center">
+        <p className="w-64 text-[#a7babb] text-center font-semibold">
           Jewellery Designed by Diana Alshehhi. Customized gold and diamonds
-          selection💎💍 Delivery worldwide 🌎Order through whatsApp 📲0505590994
+          selection Deliver worldwide Order through whatsApp +9710505590994
         </p>
       </div>
-      <div className="flex flex-col gap-3">
+      {/* <div className="flex flex-col gap-3">
         <h1 className="font-bold text-xl font-Numbers">Links</h1>
         <ul>
           <Link to={"/"}>
@@ -37,18 +39,22 @@ function Footer() {
             <li>Cart</li>
           </Link>
         </ul>
-      </div>
-      <div className="flex flex-col gap-3 text-center">
-        <h1 className="font-bold text-xl">Contact US</h1>
+      </div> */}
+      <div className="flex flex-col gap-3 text-center font-semibold">
+        {/* <h1 className="font-bold text-xl">Contact US</h1> */}
         <h1 className="flex  items-center gap-3">
           <IoLocation /> Dubai ,UAE-57068
         </h1>
         <h6 className="flex  items-center gap-3">
-          <IoCall /> +971505590994
+          <IoCall /> +971 55 872 2226
         </h6>
+        <h6 className="flex  items-center gap-3">
+          <IoCall /> +971 50 559 0994
+        </h6>
+
         <h3 className="flex items-center gap-3">
           <FaEnvelope />
-          info@Dtopaz.com
+          dianatopaz.jewellery@gmail.com
         </h3>
       </div>
     </div>
--- gitone/src/components/Pages/About.jsx
+++ gitone/src/components/Pages/About.jsx
@@ -18,7 +18,7 @@ function About() {
         className="w-full h-72 bg-[#2e728f]  flex justify-center 
       items-center text-[#c08f52]"
       >
-        <h1 className="text-5xl font-bold font-mubu">Contact us</h1>
+        <h1 className="text-5xl  font-org">Contact Us</h1>
       </div>
       <div
         className="w-full md:px-32 flex md:flex-row flex-col
@@ -42,7 +42,7 @@ function About() {
             Phone Number
           </h1>
           <p className="pl-6 text-xs w-32  font-Numbers font-bold">
-            +91-9562465095 +91-8547606322
+            +971 55 872 2226 +971 50 559 0994
           </p>
         </div>
         <div>
@@ -51,7 +51,7 @@ function About() {
             E-Mail
           </h1>
           <p className="pl-6 text-sm w-32  font-Numbers font-bold">
-            info@djmail.com
+            dianatopaz.jewellery@gmail.com
           </p>
         </div>
         <div>
@@ -60,8 +60,8 @@ function About() {
             Location
           </h1>
           <p className="pl-6 text-sm w-32  font-Numbers font-bold">
-            Yelehanka,Bangalore,
-            <br /> Karnataka,India
+            Dubai
+            <br /> UAE
           </p>
         </div>
       </div>
@@ -69,9 +69,19 @@ function About() {
         className="flex md:flex-row flex-col py-5 px-5  w-full justify-between 
       items-center pb-20 md:px-32 gap-10 md:gap-0"
       >
+        {/* <iframe
+          src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3612.073060979568!2d55.182655375379746!3d25.133220677752167!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjXCsDA3JzU5LjYiTiA1NcKwMTEnMDYuOCJF!5e0!3m2!1sen!2sin!4v1718128809770!5m2!1sen!2sin"
+          width="600"
+          height="450"
+          style="border:0;"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe> */}
         <iframe
           className=" md:w-[38rem]  md:h-[18rem] h-52 w-80 "
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31111.67912622277!2d77.62522299088258!3d12.910299474730188!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1491bfdc6ecd%3A0xf232718439fbc879!2sHSR%20Layout%2C%20Bengaluru%2C%20Karnataka!5e0!3m2!1sen!2sin!4v1716512206829!5m2!1sen!2sin"
+          src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3612.073060979568!2d55.182655375379746!3d25.133220677752167!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjXCsDA3JzU5LjYiTiA1NcKwMTEnMDYuOCJF!5e0!3m2!1sen!2sin!4v1718128809770!5m2!1sen!2sin"
+          // src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31111.67912622277!2d77.62522299088258!3d12.910299474730188!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1491bfdc6ecd%3A0xf232718439fbc879!2sHSR%20Layout%2C%20Bengaluru%2C%20Karnataka!5e0!3m2!1sen!2sin!4v1716512206829!5m2!1sen!2sin"
           width="600"
           height="450"
           allowFullScreen=""
@@ -84,9 +94,11 @@ function About() {
             follow Us on
           </h1>
           <div className="flex  w-full justify-center py-20 md:gap-10 gap-5 ">
-            <h1 className="text-4xl rounded-full border-2 p-2   border-[#15303e] cursor-pointer hover:scale-105 duration-700">
-              <FaInstagram />
-            </h1>
+            <a href="https://www.instagram.com/dtopaz.ae/">
+              <h1 className="text-4xl rounded-full border-2 p-2   border-[#15303e] cursor-pointer hover:scale-105 duration-700">
+                <FaInstagram />
+              </h1>
+            </a>
             <h1 className="text-4xl rounded-full border-2 p-2   border-[#15303e] cursor-pointer hover:scale-105 duration-700">
               <FaYoutube />
             </h1>
